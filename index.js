cca924ac408f823e391fc02a88e59eb9d2900354
Mitchell <Hmitchell.hall@gmail.com>
2021-07-28 15:30:41 -0500

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Your code here
+const dodger = document.getElementById('dodger')
+
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowLeft") {
+      moveDodgerLeft();
+    }
+  }); 
+
+document.addEventListener("keydown", function(e) {
+    if (e.key === "ArrowRight") {
+      moveDodgerRight();
+    }
+  });  
+
+
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 20}px`;
+      
+    }
+    
+  } 
+  
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+    
+  
+    if (left > 0) {
+      dodger.style.left = `${left + 20}px`;
+      
+    }
+    
+  }  
+
+      
